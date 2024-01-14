5261e1e26f97b03757d576832b809286d3ee509f
zikran69 <zikzigi69@gmail.com>
2024-01-14 17:06:19 +0700

    make custome hook

--- constants/theme.js
+++ constants/theme.js
@@ -1,7 +1,7 @@
 const COLORS = {
-  primary: "#312651",
-  secondary: "#444262",
-  tertiary: "#FF7754",
+  primary: "#001eff",
+  secondary: "#0059ff",
+  tertiary: "#0099ff",
 
   gray: "#83829A",
   gray2: "#C1C0C8",
